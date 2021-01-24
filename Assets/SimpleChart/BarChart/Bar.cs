6f12bbd8123e8369f9ec364e11ee6fbcb1c65ce7
sotanmochi <stmczk@gmail.com>
2021-01-24 12:50:16 +0900

    Add BarChart

---
+++ Assets/SimpleChart/BarChart/Bar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace SimpleChart
+{
+    public class Bar : MonoBehaviour
+    {
+        [SerializeField] Slider _BarSlider;
+        [SerializeField] Image _FillImage;
+
+        public ReactiveProperty<float> MaxValue = new ReactiveProperty<float>();
+        public ReactiveProperty<float> CurrentValue = new ReactiveProperty<float>();
+        public ReactiveProperty<Color> Color = new ReactiveProperty<Color>();
+
+        void Awake()
+        {
+            MaxValue.Value = _BarSlider.maxValue;
+            CurrentValue.Value = _BarSlider.value;
+            Color.Value = _FillImage.color;
+
+            MaxValue.Subscribe(value => 
+            {
+                // Debug.Log("Max value: " + value);
+                _BarSlider.maxValue = value;
+            })
+            .AddTo(this);
+
+            CurrentValue.Subscribe(value => 
+            {
+                // Debug.Log("Current value: " + value);
+                _BarSlider.value = value;
+            })
+            .AddTo(this);
+
+            Color.Subscribe(value => 
+            {
+                _FillImage.color = value;
+            })
+            .AddTo(this);
+        }
+    }
+}
---
+++ Assets/SimpleChart/Samples/BarChartSample.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace SimpleChart.Samples
+{
+    public class BarChartSample : MonoBehaviour
+    {
+        [SerializeField] Bar _VerticalBar;
+        [SerializeField] Bar _HorizontalBar;
+        [SerializeField] Slider _Slider_CurrentValue;
+        [SerializeField] Slider _Slider_MaxValue;
+
+        void Awake()
+        {
+            _Slider_CurrentValue.OnValueChangedAsObservable()
+            .Subscribe(value => 
+            {
+                _VerticalBar.CurrentValue.Value = value;
+                _HorizontalBar.CurrentValue.Value = value;
+            })
+            .AddTo(this);
+
+            _Slider_MaxValue.OnValueChangedAsObservable()
+            .Subscribe(value => 
+            {
+                _VerticalBar.MaxValue.Value = value;
+                _HorizontalBar.MaxValue.Value = value;
+            })
+            .AddTo(this);
+        }
+    }
+}
